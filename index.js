726b4ea0d0c0b513bf3e28807d27c60320cfd58e
Sam Gotowka <sam.gotowka@gmail.com>
2022-08-09 18:54:26 -0400

    commit tree 4df4594a6e167e6437bb9c6ab0b3b00e8ec77e04

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+function removeMain() {
+    main.remove();
+}
+removeMain()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent= 'Sam is the champion'
